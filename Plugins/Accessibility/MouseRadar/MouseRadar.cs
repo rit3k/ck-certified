01b61d9119fd4b0d793146b28c4fc68892a41134
Idriss =) <ihippocrate@gmail.com>
2013-09-30 19:21:20 +0200

    mouse rotation
    mouse translation
    bound collision

--- Plugins/Accessibility/MouseRadar/MouseRadar.cs
+++ Plugins/Accessibility/MouseRadar/MouseRadar.cs
@@ -21,50 +21,50 @@ namespace MouseRadar
         Guid PluginGuid = new Guid( PluginIdString );
         const string PluginIdVersion = "1.0.0";
         const string PluginPublicName = "MouseRadar";
-        DispatcherTimer _timerRotate;
-        DispatcherTimer _timerTranslate;
+        
         Radar _radar;
-        float _currentA;
-        int direction = 1;
+
+        
         [DynamicService( Requires = RunningRequirement.MustExistAndRun )]
         public IService<IPointerDeviceDriver> MouseDriver { get; set; }
+
+        [DynamicService( Requires = RunningRequirement.MustExistAndRun )]
+        public IService<ITriggerService> ExternalInput { get; set; }
 
         #region IPlugin Members
 
         public bool Setup( IPluginSetupInfo info )
-        {
-            _timerRotate = new DispatcherTimer();
-            _timerRotate.Interval = new TimeSpan( 5000 );
-            _timerRotate.Tick += ( o, e ) =>
-            {
-                Angle+= 0.001f;
-            };
-            _timerTranslate = new DispatcherTimer();
-            _timerRotate.Interval = new TimeSpan( 50000 );
-
-            _timerTranslate.Tick += ( o, e ) =>
-            {
-                Angle += 0.001f;
-                var p = GetTranslation( MouseDriver.Service.CurrentPointerXLocation + 1);
-                MouseDriver.Service.MovePointer((int)p.X, (int)p.Y);
-            };
-            _radar = new Radar() { DataContext = this };
-
+        {   
             return true;
         }
 
         public void Start()
         {
+            _radar = new Radar( MouseDriver.Service );
 
-            _timerRotate.Start();
-            MouseDriver.Service.PointerMove += ( o, e ) => { UpdateRadarLocation( e.X, e.Y ); };
+            ExternalInput.Service.Triggered +=  (o, e) => {
+                _radar.StartTranslation();
+            };
+            MouseDriver.Service.PointerButtonUp += ( o, e ) =>
+            {
+                _radar.StartTranslation();
+            };
+            _radar.ScreenBoundCollide += ( o, e ) => {
+                _radar.StopTranslation();
+                switch( e.ScreenBound )
+                {
+                    case ScreenBound.Left :
+                        break;
+                }
+            };
             
+            _radar.StartRotation();
             _radar.Show();
-            TranslatePointerTo( 100, 10 );
         }
 
         public void Stop()
         {
+
         }
 
         public void Teardown()
@@ -73,53 +73,7 @@ namespace MouseRadar
         }
 
         #endregion
-
-        void UpdateRadarLocation(int x, int y)
-        {
-            _radar.Left = x;
-            _radar.Top = y;
-        }
-
-        void TranslatePointerTo(int dx, int dy)
-        {
-            int sx = MouseDriver.Service.CurrentPointerXLocation;
-            int sy = MouseDriver.Service.CurrentPointerYLocation;
-
-            _currentA = (float)(dy - sy) / (float)(dx - sx);
-            if( dx - sx < 0 ) direction = -1;
-  
-            _timerTranslate.Start();
-        }
-
-        Point GetTranslation(int x)
-        {
-            return new Point( x * direction, _currentA * x );
-        }
     }
 
-    public class RadarViewModel : INotifyPropertyChanged
-    {
-        float _angle;
-
-        public float Angle 
-        {
-            get { return _angle; }
-            set 
-            {
-                _angle = value;
-                FirePropertyChanged( "Angle" );
-            }
-        }
-
-        #region INotifyPropertyChanged Members
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        #endregion
-
-        protected void FirePropertyChanged(string propertyName)
-        {
-            if( PropertyChanged != null ) PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-        }
-    }
+    
 }
---
+++ Plugins/Accessibility/MouseRadar/Radar.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Threading;
+using CommonServices;
+
+namespace MouseRadar
+{
+    public partial class Radar
+    {
+        IPointerDeviceDriver _mouseDriver;
+        int _originX;
+        int _originY;
+        int _translationDirection = 1;
+        DispatcherTimer _timerRotate;
+        DispatcherTimer _timerTranslate;
+        public event EventHandler<ScreenBoundCollideEventArgs> ScreenBoundCollide;
+        public double Slope { get; private set; }
+        public int RotationSpeed { get; set; }
+        public int TranslationSpeed { get; set; }
+        public RadarViewModel Model { get; private set; }
+        
+        public Radar(IPointerDeviceDriver pdd) : this()
+        {
+            Model = new RadarViewModel();
+            DataContext = Model;
+            _mouseDriver = pdd;
+            RotationSpeed = 1; 
+            TranslationSpeed = 1;
+
+            _timerRotate = new DispatcherTimer( DispatcherPriority.Send );
+            _timerRotate.Interval = new TimeSpan( 10000 * 17 );  //60 fps
+            _timerRotate.Tick += ( o, e ) =>
+            {
+                Model.Angle += (float)RotationSpeed / 1f;
+            };
+
+            _timerTranslate = new DispatcherTimer( DispatcherPriority.Send );
+            _timerTranslate.Interval = new TimeSpan( 10000 * 17 ); //60 fps
+            _timerTranslate.Tick += ( o, e ) =>
+            {
+                var p = GetTranslation( _mouseDriver.CurrentPointerXLocation + TranslationSpeed * _translationDirection );
+                if( CheckBoundCollision( p ) == ScreenBound.None)
+                    _mouseDriver.MovePointer( (int)p.X, (int)p.Y );
+            };
+
+            _mouseDriver.PointerMove += (o, e) => {
+                UpdateLocation( e.X, e.Y );
+            };
+        }
+
+        private ScreenBound CheckBoundCollision( Point p )
+        {
+            ScreenBound collision = ScreenBound.None;
+            if( p.X <= Screen.PrimaryScreen.Bounds.Left ) collision = ScreenBound.Left;
+            if( p.X >= Screen.PrimaryScreen.Bounds.Right ) collision = ScreenBound.Right;
+            if( p.Y <= Screen.PrimaryScreen.Bounds.Top ) collision = ScreenBound.Top;
+            if( p.Y >= Screen.PrimaryScreen.Bounds.Bottom ) collision = ScreenBound.Bottom;
+
+            if( collision != ScreenBound.None ) FireScreenBoundCollide( collision );
+            return collision;
+        }
+
+        public void StartRotation()
+        {
+            _timerRotate.Start();
+        }
+
+        public void EndRotation()
+        {
+            _timerRotate.Stop();
+        }
+
+        public void StartTranslation()
+        {
+            _originX =  _mouseDriver.CurrentPointerXLocation;
+            _originY = _mouseDriver.CurrentPointerYLocation;
+
+            double rad = Model.Angle / 180 * Math.PI;
+            Slope = Math.Tan( rad ); //Slope => pente
+
+            _translationDirection = Math.Sign( Math.Cos( rad ) );
+            _timerRotate.Stop();
+            _timerTranslate.Start();
+        }
+
+        public void StopTranslation(bool startRotation = true)
+        {
+            _timerTranslate.Stop();
+            if( startRotation ) _timerRotate.Start();
+        }
+
+        public void UpdateLocation( int x, int y )
+        {
+            Left = x - Width / 2;
+            Top = y - Height / 2;
+        }
+
+        Point GetTranslation( int x )
+        {
+            int xp = x - _originX;       //Place the coordinate system to the click zone
+            double yp = Slope * xp;     //Equation y= ax + b with b = 0
+
+            return new Point( x, yp + _originY ); //Get the computed screen coordinates
+        }
+
+        void FireScreenBoundCollide(ScreenBound bound)
+        {
+            if( ScreenBoundCollide != null ) 
+                ScreenBoundCollide(this, new ScreenBoundCollideEventArgs(bound));
+        }
+    }
+
+    [Flags]
+    public enum ScreenBound
+    {
+        None,
+        Left,
+        Top,
+        Right,
+        Bottom
+    }
+
+    public class ScreenBoundCollideEventArgs : EventArgs
+    {
+        public ScreenBound ScreenBound { get; private set; }
+
+        public ScreenBoundCollideEventArgs(ScreenBound b)
+        {
+            ScreenBound = b;
+        }
+    }
+
+    public class RadarViewModel : INotifyPropertyChanged
+    {
+        float _angle;
+
+        public float Angle
+        {
+            get { return _angle; }
+            set
+            {
+                if( value >= 360 ) _angle = value - 360;
+                else if( value < 0 ) _angle = value + 360;
+                else _angle = value;
+                FirePropertyChanged( "Angle" );
+            }
+        }
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        protected void FirePropertyChanged( string propertyName )
+        {
+            if( PropertyChanged != null ) PropertyChanged( this, new PropertyChangedEventArgs( propertyName ) );
+        }
+    }
+}
